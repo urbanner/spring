cb1eed1aee1b35d7ab395f1b3d779757f0c31c72
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-05-21 15:57:32 +0200

    Refactorings

--- src/main/java/pl/training/bank/disposition/DispositionService.java
+++ src/main/java/pl/training/bank/disposition/DispositionService.java
@@ -8,7 +8,6 @@ import pl.training.bank.account.AccountRepository;
 import pl.training.bank.operation.Operation;
 import pl.training.bank.operation.UnknownOperationException;
 
-import java.util.Arrays;
 import java.util.Map;
 
 @Service
@@ -17,10 +16,6 @@ public class DispositionService {
 
     private AccountRepository accountRepository;
     private Map<String, Operation> operations;
-
-    public void process(Disposition... dispositions) {
-        Arrays.stream(dispositions).forEach(this::process);
-    }
 
     public void process(Disposition disposition) {
         Account account = getAccount(disposition.getAccountNumber());
