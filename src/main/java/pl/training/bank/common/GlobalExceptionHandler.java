d4bb7580cb6e1d5cf1becfce3d61b37e98bae184
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-07-18 13:57:46 +0200

    MVC

---
+++ src/main/java/pl/training/bank/common/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package pl.training.bank.common;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.training.bank.account.AccountNotFoundException;
+import pl.training.bank.common.dto.ExceptionDto;
+
+import java.util.Locale;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @Setter
+    @Autowired
+    private MessageSource messageSource;
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity onException(Exception ex, Locale locale) {
+        return createResponse(ex, INTERNAL_SERVER_ERROR, locale);
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity onAccountNotFoundException(AccountNotFoundException ex, Locale locale) {
+        return createResponse(ex, NOT_FOUND, locale);
+    }
+
+    private ResponseEntity createResponse(Exception ex, HttpStatus status, Locale locale) {
+        String exceptionName = ex.getClass().getSimpleName();
+        String description;
+        try {
+            description = messageSource.getMessage(ex.getClass().getSimpleName(), null, locale);
+        } catch (NoSuchMessageException exception) {
+            description = exceptionName;
+
+        }
+        return ResponseEntity.status(status).body(new ExceptionDto(description));
+    }
+
+}
